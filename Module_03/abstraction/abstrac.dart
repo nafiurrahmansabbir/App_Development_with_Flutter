0c8f032fab0e79ea54c05cefc87c13039e9ceb60
sabbir <nafiurrahmansabbir@gmail.com>
2024-02-29 00:16:00 +0600

    added abstrc

--- Module_03/abstraction/abstrac.dart
+++ Module_03/abstraction/abstrac.dart
@@ -1,6 +1,9 @@
 abstract class Student {
   void reading();
   void join();
+  void playing() {
+    print("Playing in Abstrct Class");
+  }
 }
 
 class CseStudent extends Student {
@@ -12,6 +15,11 @@ class CseStudent extends Student {
   @override
   void join() {
     // TODO: implement join
+  }
+  @override
+  void playing() {
+    print("Playing in CSE_student Class");
+    super.playing();
   }
 }
 
@@ -27,9 +35,29 @@ class PharmecyStudent extends Student {
   }
 }
 
+// implimentation
+class xoxo implements Student {
+  @override
+  void join() {
+    // TODO: implement join
+  }
+
+  @override
+  void playing() {
+    // TODO: implement playing
+  }
+
+  @override
+  void reading() {
+    // TODO: implement reading
+  }
+}
+
 void main() {
+  print("****************Output****************\n\n");
   CseStudent cs1 = CseStudent();
   cs1.reading();
+  cs1.playing();
 
   PharmecyStudent ph1 = PharmecyStudent();
   ph1.reading();
